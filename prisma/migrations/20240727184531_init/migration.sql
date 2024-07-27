84d7ed67a96e02eb923bfedd1e1207507eb7bc75
Nicfem <sosiska2004dota2@gmail.com>
2024-07-27 22:54:23 +0300

    feat(prisma): add configuration for prisma.

---
+++ prisma/migrations/20240727184531_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "products" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "price" DOUBLE PRECISION NOT NULL,
+    "stock" INTEGER NOT NULL,
+    "category" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "imageUrl" TEXT,
+    "rating" DOUBLE PRECISION,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
